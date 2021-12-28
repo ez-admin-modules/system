dc84d9bd10015c9639ad6ad08adda19b63bcb9c6
Sirius <707759383@qq.com>
2021-12-28 15:44:16 +0800

    路由优化

--- Routes/api/admin/v1.php
+++ Routes/api/admin/v1.php
@@ -3,15 +3,12 @@
 use EzAdmin\Modules\System\Http\Controllers\Api\Admin\V1\ConfigController;
 use EzAdmin\Modules\System\Http\Controllers\Api\Admin\V1\UploadController;
 
-Route::prefix('system')->group(function () {
-    // 上传
-    Route::prefix('upload')->group(function () {
-        Route::post('image', [UploadController::class, 'image']);
-    });
-    // 配置
-    Route::get('configs', [ConfigController::class, 'index']);
-    Route::post('configs', [ConfigController::class, 'store']);
-    Route::get('configs/{config}', [ConfigController::class, 'show']);
-    Route::put('configs/{config}', [ConfigController::class, 'update']);
-    Route::delete('configs/{config}', [ConfigController::class, 'destroy']);
-});
+// 上传
+Route::post('upload/image', [UploadController::class, 'image']);
+
+// 配置
+Route::get('configs', [ConfigController::class, 'index']);
+Route::post('configs', [ConfigController::class, 'store']);
+Route::get('configs/{config}', [ConfigController::class, 'show']);
+Route::put('configs/{config}', [ConfigController::class, 'update']);
+Route::delete('configs/{config}', [ConfigController::class, 'destroy']);
